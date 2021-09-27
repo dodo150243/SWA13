cc74d26ca3ed02aa56f6dfe8df71a41efa7882cf
dodo150243 <tirawat330@gmail.com>
2021-09-27 15:51:51 +0700

    Initial commit

---
+++ src/GumballMachineTestDrive.java
@@ -0,0 +1,37 @@
+
+public class GumballMachineTestDrive {
+
+	public static void main(String[] args) {
+		GumballMachine gumballMachine = new GumballMachine(5);
+		System.out.println(gumballMachine);
+		
+		gumballMachine.intertQuarter();
+		gumballMachine.turnCrank();
+		
+		System.out.println(gumballMachine);
+		
+//		gumballMachine.intertQuarter();
+//		gumballMachine.ejectQuarter();
+//		gumballMachine.turnCrank();
+//		
+//		System.out.println(gumballMachine);
+//		
+//		gumballMachine.intertQuarter();
+//		gumballMachine.turnCrank();
+//		gumballMachine.intertQuarter();
+//		gumballMachine.turnCrank();
+//		gumballMachine.ejectQuarter();
+//		
+//		System.out.println(gumballMachine);
+//		
+//		gumballMachine.intertQuarter();
+//		gumballMachine.intertQuarter();
+//		gumballMachine.turnCrank();
+//		gumballMachine.intertQuarter();
+//		gumballMachine.turnCrank();
+//		gumballMachine.intertQuarter();
+//		gumballMachine.turnCrank();
+//		
+//		System.out.println(gumballMachine);
+	}
+}
